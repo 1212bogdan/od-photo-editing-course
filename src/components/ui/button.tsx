844174d1ca253cd831ce9c6d9a52463c6bdbc0ea
1212bogdan <ya.danulyuk@gmail.com>
2025-10-16 12:42:28 +0300

    wayforpay

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -45,10 +45,16 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
+    
+    const handleClick = () => {
+      window.location.href = "https://secure.wayforpay.com/payment/240-beauty";
+    };
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        onClick={handleClick}
         {...props}
       />
     );
